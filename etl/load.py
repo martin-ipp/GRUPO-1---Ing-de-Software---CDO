487bfd4cd9add52b2bf16203c284d81fbd4ebf77
andresagustingordon <andres.agustin.gordon@gmail.com>
2026-05-06 09:10:22 -0300

    Update load.py
    falta vincular al archivo de orgien.
    creo GEO_ID aca

--- etl/load.py
+++ etl/load.py
@@ -3,3 +3,15 @@ import pandas as pd
 file_path = ""
 file_name = "//geographic_product_demand_dataset_10M.csv"
 df = pd.read_csv(file_path&file_name)
+
+# Crear GEO_ID  (surrogate key)
+df['GEO_ID'] = (
+    df['Latitude'].astype(str) + '_' +
+    df['Longitude'].astype(str)
+)
+
+# Descarto las columnas no necesarias
+df = df.drop(columns=['City', 'State', 'Country', 'Latitude', 'Longitude'])
+
+#Cambio el formato de object a Date
+df['Date'] = pd.to_datetime(df['Date'])
