2e8e12d67231c55be7ee56bdc6958a1354638a48
andresagustingordon <andres.agustin.gordon@gmail.com>
2026-05-08 09:10:54 -0300

    Create Data.py
    Prueba para ver si podemos acceder a una onedrive
    El archivo es demasiado grande como para subirlo directamente a github (>25MB).

---
+++ data/Data.py
@@ -0,0 +1,1 @@
+dir = https://ypf-my.sharepoint.com/:x:/r/personal/ry07078_grupo_ypf_com/Documents/Ing.%20Software/geographic_product_demand_dataset_10M.csv?d=w46077d0105bc4b77a3ee55f1d7c0f593&csf=1&web=1&e=Axhez9
